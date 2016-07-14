66d81a467b5c635303d9ab6f051c1ea4317d00ad
Supriya Garg <supriyagarg@google.com>
2016-07-14 17:03:27 -0400

    Added a QueryMetadata class.
    This new class stores the metadata (i.e. headers) of a query, and lets
    users display the metadata as a table, look at the relevant metrics and
    resource descriptors, and at the individual resource and metric labels.
    
    The ResourceDescriptors class was updated to accept a list of resource
    types for filtering.

--- datalab/stackdriver/monitoring/_resource.py
+++ datalab/stackdriver/monitoring/_resource.py
@@ -27,18 +27,27 @@ class ResourceDescriptors(object):
 
   _DISPLAY_HEADERS = ('Resource type', 'Display name', 'Labels')
 
-  def __init__(self, filter_string=None, project_id=None, context=None):
+  def __init__(self, filter_string=None, types=None,
+               project_id=None, context=None):
     """Initializes the ResourceDescriptors based on the specified filters.
 
     Args:
       filter_string: An optional filter expression describing the resource
         descriptors to be returned.
+      types: A list of resource types to return the descriptors for.
       project_id: An optional project ID or number to override the one provided
           by the context.
       context: An optional Context object to use instead of the global default.
+
+    Raises:
+      ValueError: If both filter_string and types is specified.
     """
+    if not(filter_string is None or types is None):
+      raise ValueError('At most one of "filter_string" and "types" can be '
+                       'specified')
     self._client = _utils.make_client(project_id, context)
     self._filter_string = filter_string
+    self._resource_types = types
     self._descriptors = None
 
   def list(self, pattern='*'):
@@ -54,6 +63,9 @@ class ResourceDescriptors(object):
     if self._descriptors is None:
       self._descriptors = self._client.list_resource_descriptors(
           filter_string=self._filter_string)
+      if self._resource_types:
+        self._descriptors = [descriptor for descriptor in self._descriptors
+                             if descriptor.type in self._resource_types]
     return [resource for resource in self._descriptors
             if fnmatch.fnmatch(resource.type, pattern)]
 
